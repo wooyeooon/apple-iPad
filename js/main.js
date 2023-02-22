2196f3e42740ce14f6f2d267e362b33c16dbc0b0
wooYeon-gitHub <dndus9510@gmail.com>
2023-02-22 18:47:18 +0900

    Add navigation

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ const searchWrapEl = headerEl.querySelector('.search-wrap')
 const searchStarterEl = headerEl.querySelector('.search-starter')
 const searchCloserEl = searchWrapEl.querySelector('.search-closer')
 const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchInputEl = searchWrapEl.querySelector('input')
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 searchStarterEl.addEventListener('click', showSearch)
@@ -44,6 +45,9 @@ function showSearch() {
   searchDelayEls.forEach(function(el, index) {
     el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
   })
+  setTimeout(function() {
+    searchInputEl.focus()
+  }, 600)
 }
 function hideSearch() {
   headerEl.classList.remove('searching')
@@ -51,9 +55,10 @@ function hideSearch() {
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
-  searchDelayEls.revers().forEach(function(el, index) {
+  searchDelayEls.reverse().forEach(function(el, index) {
     el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
   })
   searchDelayEls.reverse()
+  searchInputEl.value = ''
 }
 
